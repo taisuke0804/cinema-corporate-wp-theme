bce7ce89b6934a472cb30dd5d335fb3a93c52ac4
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2026-03-27 17:36:47 +0900

    improvement: Service詳細ページのレイアウトとタイポグラフィを調整

--- single-service.php
+++ single-service.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-<main class="service-single">
+<main class="service-single" id="main">
 
   <?php cinema_corporate_breadcrumb(); ?>
-  
+
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
 
       <article class="service-detail">
-        <h1><?php the_title(); ?></h1>
+        <header class="service-detail-header">
+          <h1><?php the_title(); ?></h1>
+        </header>
 
-        <div class="service-content">
+        <div class="service-detail-content">
           <?php the_content(); ?>
         </div>
       </article>
